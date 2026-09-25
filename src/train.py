2155a5663d0f4bb3f1916323122f88851580956b
MrAgentBlaze <brianlala05@gmail.com>
2026-09-25 14:53:32 +0700

    Test model

--- src/train.py
+++ src/train.py
@@ -31,12 +31,12 @@ PATCH_SIZE = (32, 128, 128)
 
 BATCH_SIZE = 2
 
-NUM_EPOCHS = 50
+NUM_EPOCHS = 1
 
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 1e-5
 
-NUM_WORKERS = 0
+NUM_WORKERS = 2
 
 OUTPUT_DIR = Path("outputs") / "mednext_s"
 
@@ -121,6 +121,7 @@ train_loader = DataLoader(
     batch_size=BATCH_SIZE,
     shuffle=True,
     num_workers=NUM_WORKERS,
+    persistent_workers=True,
 )
 
 val_loader = DataLoader(
@@ -128,6 +129,7 @@ val_loader = DataLoader(
     batch_size=BATCH_SIZE,
     shuffle=False,
     num_workers=NUM_WORKERS,
+    persistent_workers=True,
 )
 
 
